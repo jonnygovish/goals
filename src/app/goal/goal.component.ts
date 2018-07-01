fc22319dd5069fd31af013e554122d11d95f7700
John <jonnygovish@gmail.com>
2018-07-01 14:04:15 +0300

    Implementing the alert service in goal component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
 import { Goals } from '../goals';
 import { GoalService } from '../goals/goal.service';
+import { AlertsService } from '../alert-service/alerts.service';
 
 @Component({
   selector: 'app-goal',
@@ -12,27 +13,30 @@ import { GoalService } from '../goals/goal.service';
 export class GoalComponent implements OnInit {
 
   goals:Goal[];
-  constructor(goalService:GoalService){
-    this.goals = goalService.getGoals()
-  }
-
+  alertService: AlertsService;
+  
   addNewGoal(goal){
     let goalLength = this.goals.length;
     goal.id=goalLength+1;
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
   }
-
+  
   deleteGoal(isComplete, index){
     if(isComplete){
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
-
+      
       if (toDelete){
         this.goals.splice(index,1);
+        this.alertService.alertMe("Goal has been deleted")
       }
     }
   }
-
+  
+  constructor(goalService:GoalService, alertService:AlertsService){
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
   toogleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
